0ae463d852dc7143d0de895eff01a1adc81699a9
jadeilton <jadeiltondm@gmail.com>
2025-08-24 22:02:32 -0300

    Continuação

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { LinksComponent } from "../../components/links/links.component";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [LinksComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
